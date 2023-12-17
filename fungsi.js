b0d0cd0b114533fddb316d73c89c22a8292ecf3d
MOH ROOBI AL FURQON <rooby.muhammad@gmail.com>
2023-12-17 09:57:55 +0700

    penambahan perkalian

--- fungsi.js
+++ fungsi.js
@@ -1,4 +1,8 @@
-console.log('Fungsi dasar');
-
 const add = (a,b) => a + b;
 console.log(add(1,2));
+
+const perkalian = (a, b) => a * b;
+console.log(perkalian(2, 2)); 
+
+const pembagian = (a, b) => a - b;
+console.log(pembagian(3,3));
