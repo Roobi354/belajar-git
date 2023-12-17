bebd01d9238e04e51949501ba77265bb817a2254
MOH ROOBI AL FURQON <rooby.muhammad@gmail.com>
2023-12-17 10:08:17 +0700

    penambahan pembagian

--- fungsi.js
+++ fungsi.js
@@ -1,8 +1,11 @@
-const add = (a,b) => a + b;
-console.log(add(1,2));
+const penambahan = (a,b) => a + b;
+console.log(penambahan(1,2));
 
 const perkalian = (a, b) => a * b;
 console.log(perkalian(2, 2)); 
 
-const pembagian = (a, b) => a - b;
+const pembagian = (a, b) => a / b;
+console.log(pembagian(3,3));
+
+const pengurangan = (a, b) => a - b;
 console.log(pembagian(3,3));
